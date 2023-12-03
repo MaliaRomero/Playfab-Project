dab5ece4f4e5d35afd8e74521c5a199b203e5d9c
MaliaRomero <romerm15@my.erau.edu>
2023-12-02 22:12:57 -0700

    Revert "Adding Backrooms"
    This reverts commit 0f2258f9a8d089157d89c5e74766d8df651f2e08.

--- Playfab/Assets/Scripts/PlayerController.cs
+++ Playfab/Assets/Scripts/PlayerController.cs
@@ -18,8 +18,6 @@ public class PlayerController : MonoBehaviour
     public GameObject playButton;
     public TextMeshProUGUI curTimeText;
 
-    public float rotationSpeed = 150f;
-
     void Awake()
     {
         rig = GetComponent<Rigidbody>();
@@ -30,36 +28,12 @@ public class PlayerController : MonoBehaviour
         if (!isPlaying)
             return;
 
-        // Rotate left when 'Q' is pressed
-        if (Input.GetKey("q"))
-        {
-            RotatePlayer(-5);
-
-        }
-        // Rotate right when 'E' is pressed
-        else if (Input.GetKey("e"))
-        {
-            RotatePlayer(5);
-        }
-
         float x = Input.GetAxis("Horizontal") * speed;
         float z = Input.GetAxis("Vertical") * speed;
 
-        // Calculate movement direction based on both input and current rotation
-        Vector3 moveDirection = new Vector3(x, 0f, z).normalized;
-
-        // Calculate the rotation amount based on the current rotation
-        Quaternion rotation = Quaternion.Euler(0f, transform.eulerAngles.y, 0f);
-
-        // Apply rotation to the movement direction
-        moveDirection = rotation * moveDirection;
-
-        // Apply movement to the rigidbody
-        rig.velocity = new Vector3(moveDirection.x * speed, rig.velocity.y, moveDirection.z * speed);
+        rig.velocity = new Vector3(x, rig.velocity.y, z);
 
         curTimeText.text = (Time.time - startTime).ToString("F2");
-
-
     }
 
     void OnTriggerEnter(Collider other)
@@ -86,16 +60,6 @@ public class PlayerController : MonoBehaviour
         timeTaken = Time.time - startTime;
         isPlaying = false;
         Leaderboard.instance.SetLeaderboardEntry(-Mathf.RoundToInt(timeTaken * 1000.0f));
-        playButton.SetActive(true);    
-    }
-
-    void RotatePlayer(int direction)
-    {
-        // Calculate the rotation amount based on the direction
-        float rotationAmount = direction * rotationSpeed * Time.deltaTime;
-
-        // Apply the rotation to the player's transform
-        transform.Rotate(Vector3.up, rotationAmount);
-    }
+        playButton.SetActive(true);    }
 
 }
